c92a691132eeadef1e17f3828d3d86f7e43b4144
Metanat <m.hsadat@gmail.com>
2024-09-19 00:24:12 -0700

    Added Serializer and Serializer test

--- metamidi/src/Serializer.java
+++ metamidi/src/Serializer.java
@@ -2,35 +2,51 @@ import java.util.*;
 
 public class Serializer {
 
-    private static final int PRE_PIANO_BIT = 0;
-    private static final int PRE_NOTE_BIT = 1;
+    private static final boolean PRE_PIANO_BIT = false;
+    private static final boolean PRE_NOTE_BIT = true;
+    public static final int PIANO_TYPE_BIT_COUNT = 3;
 
-    public void serialize(final Map<FNEH_Piano, Map<Integer, Integer>> activeNotes) {
-        List<Byte> bytes = new ArrayList<>();
-        activeNotes.forEach(this::serialize);
+    public byte[] serialize(final Map<FNEH_Piano, Set<Integer>> activeNotes) {
+
+        int totalSizeNeeded = activeNotes.entrySet().stream().map(entry -> bitsRequiredToCode(entry.getKey(), entry.getValue()))
+                .reduce(Integer::sum).orElse(0);
+
+        BitSet bitSet = new BitSet(totalSizeNeeded);
+        int sizeSoFar = totalSizeNeeded - 1;
+
+        for (Map.Entry<FNEH_Piano, Set<Integer>> entry : activeNotes.entrySet()) {
+            sizeSoFar = serialize(entry.getKey(), entry.getValue(), bitSet, sizeSoFar);
+        }
+
+        return bitSet.toByteArray();
+
     }
 
-    private BitSet serialize(FNEH_Piano fnehPiano, Map<Integer, Integer> activeNotes) {
-        int numberOfBits = 1 // bit required to encode piano or key
+    private int serialize(FNEH_Piano fnehPiano, Set<Integer> activeNotes, BitSet bitSet, int sizeSoFar) {
+
+        bitSet.set(sizeSoFar, PRE_PIANO_BIT);
+        sizeSoFar --;
+        setBitsAt(bitSet, sizeSoFar, encodePiano(fnehPiano), PIANO_TYPE_BIT_COUNT);
+        sizeSoFar -= PIANO_TYPE_BIT_COUNT;
+
+        for (int note : activeNotes) {
+            sizeSoFar = addNoteToBitSet(bitSet, sizeSoFar, note, fnehPiano);
+        }
+        return sizeSoFar;
+    }
+
+    private int bitsRequiredToCode(FNEH_Piano piano, Set<Integer> activeNotes) {
+        return 1 // bit required to encode piano or key
+                + 3 //bits requires to encode the piano itself
                 + activeNotes.size()
                 * (1         //bit required to encode it's a key
-                + numberOfBitsPerNotOf(fnehPiano));
+                + numberOfBitsPerNotOf(piano));
+    }
 
-        BitSet bitSet = new BitSet(numberOfBits);
-
-        bitSet.set(0, PRE_PIANO_BIT);
-        bitSet.set(1, encodePiano(fnehPiano));
-
-        int bitsSoFar = 4;
-        bitSet.set(bitsSoFar, );
-        int code = PRE_PIANO_BIT;
-
-        code = PRE_NOTE_BIT << 3; //make room to add the piano code
-        code = code |
-
-
-
-        return bitSet;
+    private int addNoteToBitSet(BitSet bitSet, int sizeSoFar, int activeNote, FNEH_Piano fnehPiano) {
+        bitSet.set(sizeSoFar, PRE_NOTE_BIT);
+        setBitsAt(bitSet, sizeSoFar - 1, activeNote, numberOfBitsPerNotOf(fnehPiano));
+        return sizeSoFar - 1 - numberOfBitsPerNotOf(fnehPiano);
     }
 
     private int encodePiano(FNEH_Piano piano) {
@@ -51,21 +67,19 @@ public class Serializer {
         };
     }
 
-    private static BitSet setBitsAt(int number, int size) {
-        if (number >= 1 << size) {
+    private void setBitsAt(BitSet bitset, int sizeSoFar, int number, int bitCount) {
+        if (number >= 1 << bitCount) {
             throw new IndexOutOfBoundsException("Can only cover up to 7 pianos. " +
                     "This is just a limit in the encoding and can be increased. " +
                     "Please reach out to me to help.");
         }
-        BitSet bitSet = new BitSet(size);
-
-        for (int i = 0; i < size; i++) {
-            if ((number & (1 << i)) != 0) {
-                bitSet.set(i);
+        int indexer = 1 << (bitCount - 1);
+        for (int i = 0; i < bitCount; i++) {
+            if ((number & indexer) != 0) {
+                bitset.set(sizeSoFar - i);
             }
+            indexer = indexer >> 1;
         }
-
-        return bitSet;
     }
 
 
---
+++ metamidi/tst/SerializerTest.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SerializerTest {
+
+    @Test
+    void seriaize() {
+        Map<FNEH_Piano, Set<Integer>> activeNotes = new HashMap<>();
+        activeNotes.put(FNEH_Piano.N_OVATION_25_KEY, Set.of(16, 18, 20, 1, 3));
+        activeNotes.put(FNEH_Piano.MPK_MINI_25_KEY, Set.of(2, 4, 6, 20, 24));
+        activeNotes.put(FNEH_Piano.FNEH_88_KEY, Set.of(0, 87, 84, 83, 12, 1, 2, 3, 10));
+        activeNotes.put(FNEH_Piano.FNEH_49_KEY, Set.of(0, 47, 48, 46, 12, 1, 2, 5, 9));
+        activeNotes.put(FNEH_Piano.FNEH_63_KEY, Set.of(0, 22, 55, 46, 62, 1, 2, 5, 9));
+        Serializer serializer = new Serializer();
+        byte[] serialize = serializer.serialize(activeNotes);
+        System.out.println(activeNotes);
+        System.out.println(Arrays.toString(serialize));
+    }
+
+    @Test
+    void seriaize_empty() {
+        Map<FNEH_Piano, Set<Integer>> activeNotes = new HashMap<>();
+        Serializer serializer = new Serializer();
+        byte[] serialize = serializer.serialize(activeNotes);
+        assertEquals(0, serialize.length);
+    }
+
+    @Test
+    void seriaize_justOneNote() {
+        Map<FNEH_Piano, Set<Integer>> activeNotes = new HashMap<>();
+        activeNotes.put(FNEH_Piano.N_OVATION_25_KEY, Set.of(1));
+        Serializer serializer = new Serializer();
+        byte[] serialize = serializer.serialize(activeNotes);
+        print(serialize);
+    }
+
+    @Test
+    void seriaize_justOneNoteOf0() {
+        Map<FNEH_Piano, Set<Integer>> activeNotes = new HashMap<>();
+        activeNotes.put(FNEH_Piano.FNEH_88_KEY, Set.of(0));
+        Serializer serializer = new Serializer();
+        byte[] serialize = serializer.serialize(activeNotes);
+        System.out.println(Arrays.toString(serialize));
+        print(serialize);
+    }
+
+    private static void print(byte[] serialize) {
+        for (byte b : serialize) {
+            System.out.println(String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0'));
+        }
+    }
+
+
+}

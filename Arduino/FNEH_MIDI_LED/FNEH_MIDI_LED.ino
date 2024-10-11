e5ab8e3bc7f3d059cef89d1deeec88cbee5275f9
Hooshsadat, Metanat <metanat@amazon.com>
2024-10-11 10:35:31 -0700

    Added a debug flag and removed all unused functions.

--- Arduino/FNEH_MIDI_LED/FNEH_MIDI_LED.ino
+++ Arduino/FNEH_MIDI_LED/FNEH_MIDI_LED.ino
@@ -48,6 +48,8 @@ Starts with large white note + a 2w1b and ends with a large white note
 #define N_OVATION_25_SMALLEST_NOTE 21
 #define MPK_MINI_25_SMALLEST_NOTE 21
 
+#define debug false
+
 CRGB leds_88_key[FNEH_88_LED_COUNT];
 CRGB leds_63_key[FNEH_63_LED_COUNT];
 CRGB leds_49_key[FNEH_49_LED_COUNT];
@@ -120,23 +122,24 @@ void setup()
 
   FastLED.show();
 
-  //Flash 10 LEDs of each strip so we know they are ready. //TODO: Remove this block so if things reconnect mid show lights don't flash
-  for (Keyboard keyboard : keyboards) {
-    CRGB* leds = keyboard.leds;
-    byte numLeds = keyboard.ledCount;
-    byte i = 0;
-    for (i ; i < 10; i++) {      
-      fill_gradient_RGB(leds, i,myColor, i+1,myColor);
-      FastLED.show();
-      delay(2);
-      if (i >= 1) {
-        fill_gradient_RGB(leds, i-1,CRGB::Black, i,CRGB::Black);
+  //Flash 10 LEDs of each strip so we know they are ready.
+  if(debug) {
+    for (Keyboard keyboard : keyboards) {
+      CRGB* leds = keyboard.leds;
+      byte numLeds = keyboard.ledCount;
+      byte i = 0;
+      for (i ; i < 10; i++) {      
+        fill_gradient_RGB(leds, i,myColor, i+1,myColor);
         FastLED.show();
         delay(2);
+        if (i >= 1) {
+          fill_gradient_RGB(leds, i-1,CRGB::Black, i,CRGB::Black);
+          FastLED.show();
+          delay(2);
+        }
       }
+      fill_gradient_RGB(leds, i,CRGB::Black, i,CRGB::Black);
     }
-    fill_gradient_RGB(leds, i,CRGB::Black, i,CRGB::Black);
-
  }
 
   // Listen for MIDI messages on all channels, each channel represents a keyboard.
@@ -144,25 +147,6 @@ void setup()
 
   MIDI.setHandleNoteOn(OnNoteOn);
   MIDI.setHandleNoteOff(OnNoteOff);
-
-  // We don't have a use for the following functions, we are just logging messages for testing. TODO: Remove so we are not doing unnecessary stuff.
-  MIDI.setHandleAfterTouchPoly(OnAfterTouchPoly);
-  MIDI.setHandleControlChange(OnControlChange);
-  MIDI.setHandleProgramChange(OnProgramChange);
-  MIDI.setHandleAfterTouchChannel(OnAfterTouchChannel);
-  MIDI.setHandlePitchBend(OnPitchBend);
-  MIDI.setHandleSystemExclusive(OnSystemExclusive);
-  MIDI.setHandleTimeCodeQuarterFrame(OnTimeCodeQuarterFrame);
-  MIDI.setHandleSongPosition(OnSongPosition);
-  MIDI.setHandleSongSelect(OnSongSelect);
-  MIDI.setHandleTuneRequest(OnTuneRequest);
-  MIDI.setHandleClock(OnClock);
-  MIDI.setHandleStart(OnStart);
-  MIDI.setHandleContinue(OnContinue);
-  MIDI.setHandleStop(OnStop);
-  MIDI.setHandleActiveSensing(OnActiveSensing);
-  MIDI.setHandleSystemReset(OnSystemReset);
-
 }
 
 // -----------------------------------------------------------------------------
@@ -180,29 +164,31 @@ void loop()
 // -----------------------------------------------------------------------------
 static void OnNoteOn(byte channel, byte note, byte velocity) {
 
-  //TODO: Add an if block so we will skip all the serial print lines.
-  Serial.print(F("NoteOn  from channel: "));
-  Serial.print(channel);
-  Serial.print(F(", note: "));
-  Serial.print(note);
-  Serial.print(F(", velocity: "));
-  Serial.print(velocity);
-
-
-  Serial.print(F(", keyboad led count is: "));
-  Serial.println(keyboardForChannel(channel).ledCount);
+  if(debug) {
+    Serial.print(F("NoteOn  from channel: "));
+    Serial.print(channel);
+    Serial.print(F(", note: "));
+    Serial.print(note);
+    Serial.print(F(", velocity: "));
+    Serial.print(velocity);
+    Serial.print(F(", keyboad led count is: "));
+    Serial.println(keyboardForChannel(channel).ledCount);
+  }
 
   alterLEDs(channel, myColor, note);
 
 }
 
 static void OnNoteOff(byte channel, byte note, byte velocity) {
-  Serial.print(F("NoteOff from channel: "));
-  Serial.print(channel);
-  Serial.print(F(", note: "));
-  Serial.print(note);
-  Serial.print(F(", velocity: "));
-  Serial.println(velocity);
+
+  if(debug) {
+    Serial.print(F("NoteOff from channel: "));
+    Serial.print(channel);
+    Serial.print(F(", note: "));
+    Serial.print(note);
+    Serial.print(F(", velocity: "));
+    Serial.println(velocity);
+  }
 
   alterLEDs(channel, CRGB::Black, note);
 }
@@ -234,90 +220,4 @@ void alterLEDs(byte channel, CRGB myColor, byte note) {
   }
 
   FastLED.show();   
-}
-
-static void OnAfterTouchPoly(byte channel, byte note, byte pressure) {
-  Serial.print(F("AfterTouchPoly from channel: "));
-  Serial.print(channel);
-  Serial.print(F(", note: "));
-  Serial.print(note);
-  Serial.print(F(", pressure: "));
-  Serial.println(pressure);
-}
-
-static void OnControlChange(byte channel, byte number, byte value) {
-  Serial.print(F("ControlChange from channel: "));
-  Serial.print(channel);
-  Serial.print(F(", number: "));
-  Serial.print(number);
-  Serial.print(F(", value: "));
-  Serial.println(value);
-}
-
-static void OnProgramChange(byte channel, byte number) {
-  Serial.print(F("ProgramChange from channel: "));
-  Serial.print(channel);
-  Serial.print(F(", number: "));
-  Serial.println(number);
-}
-
-static void OnAfterTouchChannel(byte channel, byte pressure) {
-  Serial.print(F("AfterTouchChannel from channel: "));
-  Serial.print(channel);
-  Serial.print(F(", pressure: "));
-  Serial.println(pressure);
-}
-
-static void OnPitchBend(byte channel, int bend) {
-  Serial.print(F("PitchBend from channel: "));
-  Serial.print(channel);
-  Serial.print(F(", bend: "));
-  Serial.println(bend);
-}
-
-static void OnSystemExclusive(byte * array, unsigned size) {
-  Serial.println(F("SystemExclusive"));
-}
-
-static void OnTimeCodeQuarterFrame(byte data) {
-  Serial.print(F("TimeCodeQuarterFrame: "));
-  Serial.println(data, HEX);
-}
-
-static void OnSongPosition(unsigned beats) {
-  Serial.print(F("SongPosition: "));
-  Serial.println(beats);
-}
-
-static void OnSongSelect(byte songnumber) {
-  Serial.print(F("SongSelect: "));
-  Serial.println(songnumber);
-}
-
-static void OnTuneRequest() {
-  Serial.println(F("TuneRequest"));
-}
-
-static void OnClock() {
-  Serial.println(F("Clock"));
-}
-
-static void OnStart() {
-  Serial.println(F("Start"));
-}
-
-static void OnContinue() {
-  Serial.println(F("Continue"));
-}
-
-static void OnStop() {
-  Serial.println(F("Stop"));
-}
-
-static void OnActiveSensing() {
-  Serial.println(F("ActiveSensing"));
-}
-
-static void OnSystemReset() {
-  Serial.println(F("SystemReset"));
 }
